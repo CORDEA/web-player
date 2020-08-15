3c418eed12a50f9317db6a59ba6b592844a989db
Yoshihiro Tanaka <contact@cordea.jp>
2020-08-15 16:26:40 +0900

    apply reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,8 @@
 import {combineReducers} from "redux";
+import playerReducer from "./PlayerReducer";
 
-export const rootReducer = combineReducers({})
+export const rootReducer = combineReducers({
+  player: playerReducer
+})
 
 export type RootState = ReturnType<typeof rootReducer>
