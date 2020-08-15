efff69128a3884256cfed7b8ce9f47b866fb14ea
Yoshihiro Tanaka <contact@cordea.jp>
2020-08-15 16:34:56 +0900

    handle drag and drop

--- src/DropView.tsx
+++ src/DropView.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import MaterialIcon from "@material/react-material-icon";
 import "./DropView.css";
+import {connect, ConnectedProps} from "react-redux";
+import {dropFile} from "./store/actions";
 
-function DropView() {
-  return (
-    <div className="DropView">
-      <MaterialIcon className="DropView-icon" icon="audiotrack" />
-    </div>
-  )
+const mapDispatch = {
+  drop: (files: FileList) => dropFile(files)
 }
 
-export default DropView;
+const connector = connect(null, mapDispatch)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+type Props = PropsFromRedux
+
+const DropView = (props: Props) => (
+  <div
+    className="DropView"
+    draggable="true"
+    onDragOver={(e) => {
+      e.preventDefault()
+      e.dataTransfer.dropEffect = "copy"
+    }}
+    onDrop={(e) => {
+      e.preventDefault()
+      props.drop(e.dataTransfer.files)
+    }}>
+    <MaterialIcon className="DropView-icon" icon="audiotrack"/>
+  </div>
+)
+
+export default connector(DropView);
