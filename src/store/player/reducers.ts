ed6e55ca82cb2f81054c037b5454570a156484de
Yoshihiro Tanaka <contact@cordea.jp>
2020-08-16 14:12:35 +0900

    play music

--- src/store/player/reducers.ts
+++ src/store/player/reducers.ts
@@ -1,4 +1,5 @@
 import {PlayerActionTypes, PlayerState} from "./types";
+import {Howl} from "howler";
 
 const initialState: PlayerState = {
   isPlaying: false,
@@ -8,7 +9,17 @@ const initialState: PlayerState = {
 function playerReducer(state = initialState, action: PlayerActionTypes): PlayerState {
   switch (action.type) {
     case "PLAY": {
-      return state
+      if (state.musics.length === 0) {
+        return state
+      }
+      const sound = new Howl({
+        src: [state.musics[0].url]
+      })
+      sound.play()
+      return {
+        ...state,
+        isPlaying: true
+      }
     }
     case "PAUSE": {
       return state
